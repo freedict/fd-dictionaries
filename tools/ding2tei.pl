26121c4e26b060af2e11f9a00a5e464d17639be8
horst <horst@e3372f6d-cdc2-4eab-8ac2-e1627c5befe3>
2000-04-06 05:14:47 +0000

    This commit was generated by cvs2svn to compensate for changes in r10, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/freedict/code/trunk@11 e3372f6d-cdc2-4eab-8ac2-e1627c5befe3

---
+++ tools/ding2tei.pl
@@ -0,0 +1,87 @@
+#!/usr/bin/perl
+# 
+# this program converts a ding database from stdin
+# and converts it to tei dictionary format on stdout.
+# no sorting whatsoever is done. 
+# also the heaader & footer are just basic shells
+#
+# GPL´ed 2000 by H.Eyermann
+#
+#
+
+$rev = "0";
+
+# first print the header
+print<<EOF;
+<!DOCTYPE TEI.2       PUBLIC "-//TEI P3//DTD Main Document Type//EN"  [
+   <!ENTITY % TEI.dictionaries "INCLUDE" >
+]>
+
+<tei.2>
+  <teiheader>
+    <filedesc>
+      <titlestmt>
+        <title>           </title>
+      </titlestmt>
+      <publicationstmt>
+        <authority>Freedict.de</authority>
+      </publicationstmt>
+      <sourcedesc>
+        <p>http://www.freedict.de</p>
+      </sourcedesc>
+    </filedesc>
+  </teiheader>
+  <text>
+    <body>
+EOF
+
+# now we can read the database from stdin
+
+while (<>) {
+  ($ger, $eng) = split "::", $_ ;
+  if ($rev == "1") {
+    $temp = $eng;
+    $eng = $ger;
+    $ger = $temp;
+  }
+
+  chomp $eng;
+  $ger =~ s/^\s*//;
+  $eng =~ s/^\s*//;
+    foreach $ge (split ",", $ger) {
+       # match: character - non character followed by nothing, or brackets with text included
+      $ge =~ m/{(\S*)}/;
+      $gender = $1;
+      $pos = (($gender ne "") ? "<pos>n</pos>" : ""); #  if ($gender ne "");
+      if ($gender eq "pl")  {
+        $gram1 = "<num>pl</num>";
+      } else {
+        $gram1 = "<num>s</num><gen>$gender</gen>";
+      }
+
+      $po= (( $pos ne "") ? "\n        <gramgrp>\n          $pos$gram1\n        </gramgrp>": "");  #  if ($pos ne "");
+      $ge =~ s/{\S*}//;  # remove pos in definition
+      $eng =~ s/{\S*}//g;  # remove pos in definition
+      $ge =~ s/^\s//;    # delete leading spaces
+      $eng =~ s/^\s//;    # delete leading spaces
+      $ge =~ s/\s*$//;   # delete tailing spaces
+      $eng=~ s/\s*$//;   # delete tailing spaces
+
+print<<EOF;
+      <entry>
+        <form>
+          <orth>$ge</orth>
+        </form>$po
+        <trans>$eng</trans>
+      </entry>
+EOF
+    }
+}
+
+
+# finaly print the footer:
+print<<EOF
+    </body>
+  </text>
+</tei.2>
+EOF
