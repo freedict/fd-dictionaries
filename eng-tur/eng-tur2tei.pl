e11df887f36ae5218908b280883827b871e465e6
micha137 <micha137@e3372f6d-cdc2-4eab-8ac2-e1627c5befe3>
2005-11-07 18:14:11 +0000

    updates to build nicely and automatically, .tei file not in cvs anymore
    git-svn-id: https://svn.code.sf.net/p/freedict/code/trunk@433 e3372f6d-cdc2-4eab-8ac2-e1627c5befe3

--- eng-tur/eng-tur2tei.pl
+++ eng-tur/eng-tur2tei.pl
@@ -1,39 +1,53 @@
 #!/usr/bin/perl
 
-open(E, "<ingtur-ing.utf8") || die("can't open ingtur-ing: $!");
-open(T, "<ingtur-tur.utf8") || die("can't open ingtur-tur: $!");
+open(E, "<:utf8", "ingtur-ing.utf8") || die("can't open ingtur-ing: $!");
+open(T, "<:utf8", "ingtur-tur.utf8") || die("can't open ingtur-tur: $!");
+binmode(STDOUT, ":utf8");
+
+sub escape
+{
+  my $s = shift;
+  $s =~ s/&/&amp;/;
+  return $s;
+}
 
 while(<E>)
 {
   $e = $_;
-  $e =~ s/\s*\r\n//;# remove trailing space & newlines
+  $e =~ tr/\x{FEFF}//;# remove Byte Order Mark
+  $e =~ s/\s*\r\n//;# remove trailing space & newline
+
   $t = <T>;
+  # `ingtur-tur.utf' contains an invalid unicode character, which
+  # leads to `make validation` complaining, if it stays unreplaced:
+  # > nsgmls:eng-tur.tei:136233:13:E: non SGML character number 149
+  $t =~ tr/\x{95}\x{FEFF}/'/;# remove Byte Order Mark
   $t =~ s/\s*\r\n//;
   $t =~ s/^\s*//;# remove leading space
-  
+
   @senses = split /;/, $t;
   
   print "<entry>\n";
-  print "     <form>\n";                                                          
-  print "       <orth>$e</orth>\n";
-  print "     </form>\n";                                                       
-  $s = 0;  
+  print "     <form>\n";
+  print '       <orth>' . escape($e) . "</orth>\n";
+  print "     </form>\n";
+  $s = 0;
   foreach(@senses)
   {
     $s++;
     s/\s*$//;# remove trailing space
     s/^\s*//;# remove leading space
-    print "     <sense n=\"$s\">\n";                                                         
-    print "       <trans>\n";                                                         
+    print "     <sense n=\"$s\">\n";
+    print "       <trans>\n";
     @equiv = split /,/;
     foreach(@equiv)
     {
       s/\s*$//;# remove trailing space
       s/^\s*//;# remove leading space
-      print "         <tr>$_</tr>\n";
+      print '         <tr>' . escape($_) . "</tr>\n";
     }
     print "       </trans>\n";
-    print "     </sense>\n";                                                         
+    print "     </sense>\n";
   }
   print "   </entry>\n";
 }
