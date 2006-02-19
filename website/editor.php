35d5b86f2f558049cde53e87cf1b35a151160914
micha137 <micha137@e3372f6d-cdc2-4eab-8ac2-e1627c5befe3>
2006-02-19 17:36:43 +0000

    integrate a Debian repository into the website
    git-svn-id: https://svn.code.sf.net/p/freedict/code/trunk@507 e3372f6d-cdc2-4eab-8ac2-e1627c5befe3

--- website/editor.php
+++ website/editor.php
@@ -31,3 +31,9 @@ cvs -z3 -d:pserver:anonymous@cvs.sourceforge.net:/cvsroot/freedict co -P freedic
 
 <p><?php printf(_("For the %srequirements%s, please also refer to the documentation."),
   '<a href="editordoc.html#freedict-editor-requirements">', '</a>') ?></p>
+
+<p><?php printf(_("If you use %sDebian%s unstable, you can add this to your %s:"),
+  '<a href="http://debian.org/">', '</a>', '<tt>/etc/apt/sources.list</tt>') ?></p>
+
+<blockquote><small><tt>deb <a href="http://freedict.org/debian-repository">http://freedict.org/debian-repository</a> binary/<br>
+deb-src <a href="http://freedict.org/debian-repository">http://freedict.org/debian-repository</a> source/</tt></small></blockquote>
