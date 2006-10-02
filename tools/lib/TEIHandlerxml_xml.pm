57c1eea68f2aa99e97f9ed3d74d72418bcd37862
micha137 <micha137@e3372f6d-cdc2-4eab-8ac2-e1627c5befe3>
2006-10-02 20:27:27 +0000

    support use of onsgmls, fix xmltei2xmldict.pl by adding an error handler
    git-svn-id: https://svn.code.sf.net/p/freedict/code/trunk@528 e3372f6d-cdc2-4eab-8ac2-e1627c5befe3

--- tools/lib/TEIHandlerxml_xml.pm
+++ tools/lib/TEIHandlerxml_xml.pm
@@ -1,5 +1,8 @@
 # SAX handler module for tei2dictxml_xml converter
 
+# V1.3 10/2006 Michael Bunk
+#	* removed trailing white space
+#	* added error handler, so XML:ESISParser doesn't die() on warnings
 # V1.2 5/2004 Michael Bunk
 #	* added option to generate 00-database-allchars header
 #	* if reverse index is generated, 00-database-short will indicate it now
@@ -14,14 +17,14 @@
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software 
+# along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 # thinking about event & calling order of the Dict subs (there is no other doc?):
@@ -87,7 +90,7 @@ sub _escape {
     my $string = shift;
 
     $string =~ s/([\x09\x0a\x0d&<>\"])/$char_entities{$1}/ge;
- 
+
     return $string;
     }
 
@@ -112,14 +115,14 @@ sub dumpTag {
     foreach(keys %attrhash) {
      $attr .= ' '.$_.'="'._escape($attrhash{$_}).'"';
      }
-    
+
     return "<".$element->{Name}.$attr.">";
 
 }
 
 sub apply_filter_cmd {
     my ($filtercommand, $quoted) = @_;
-    
+
     if($filtercmd ne "") {
         print STDERR ".";
         #print "calling open2 with '$filtercmd'...";
@@ -148,7 +151,7 @@ sub apply_filter_cmd {
 
 #	print STDERR "\ncalling getResultArg...";
 	$quoted = $sab->getResultArg('arg:/output');
-	
+
 #	print STDERR "\ngot: $quoted\n\n\n\n";
 	}
 
@@ -159,7 +162,7 @@ sub set_options {
     my ($self, $filename, $askip_header, $aGenerate00DatabaseUtf8, $aLocale,
       $aFilterCmd, $aStyleSheet, $aReverseindex, $aAllchars) = @_;
     $file_name = $filename;
-    $file_name =~ s/^(\w*\/)+//g;         
+    $file_name =~ s/^(\w*\/)+//g;
     $file_name =~ s/(\S*)\.\w*/$1/;
     $skip_header=$askip_header;
     $filtercmd=$aFilterCmd;
@@ -173,27 +176,27 @@ sub set_options {
 	}
 
     Dict->open_dict($file_name,$aLocale,$aGenerate00DatabaseUtf8);
-    
+
     if($aGenerate00DatabaseUtf8)
     {
 	print STDERR "Generating 00-database-utf8\n";
     	Dict::set_headword(); Dict::add_text("00-database-utf8");
 	Dict::write_text(); Dict::end_headword();
-	
+
 	# might not be needed at all, but we give a hint
         Dict::write_direct("\n  ");
 	Dict::write_direct("This dictionary is UTF-8 encoded. If you use dictd, ".
 		           "make sure to start it with the appropriate --locale option.");
         Dict::write_newline();
     }
-    
+
     if($aAllchars)
     {
 	print STDERR "Generating 00-database-allchars\n";
     	Dict::set_headword(); Dict::add_text("00-database-allchars");
 	Dict::write_text(); Dict::end_headword();
         Dict::write_newline();
-    }	
+    }
 }
 
 sub start_element {
@@ -204,8 +207,8 @@ sub start_element {
 
     # for multiple orths at beginning of <form>
     if (!$reverse_index && ($Dict::head) && ($part ne "orth")) { Dict::end_headword() };
-    
-    
+
+
     # see what tag we have (& remember XML is case sensitive!)
     # most frequent tags on top
     if (($part eq "entry") ||
@@ -213,19 +216,19 @@ sub start_element {
 	$quoted = "";
 	$state = "quoting";
 	}
-	
+
     elsif ($part eq "orth" && !$reverse_index) {
         if ($preLastStartTag eq "form") {Dict::set_headword()}
 	$state = "orth";$current_headword='';
 	}
-	
+
     elsif ($part eq "tr" && $reverse_index) {
         if (!$Dict::head) {Dict::set_headword()}
 	$state = "tr";$current_headword='';
 	}
 
-	
-    if ($part eq "revisionDesc") {	
+
+    if ($part eq "revisionDesc") {
 	  # add myself as
 	  # <change>
 	  #  <date>now</date>
@@ -241,7 +244,7 @@ sub start_element {
 	  }
     elsif (($state eq "quoting") ||
         ($state eq "orth") ||
-	($state eq "tr")) { 
+	($state eq "tr")) {
 	  $quoted .= dumpTag($element);
 	  }
 
@@ -257,7 +260,7 @@ sub start_element {
 sub characters {
     my ($self, $element) = @_;
     my $data = $element->{Data};
-    
+
     # FIXME: maybe with other parser than ESISParser?
     # my guess: sometimes our loved ESISParser calls this handler
     # while it shouldn't: to notify us of a line break and white
@@ -266,8 +269,8 @@ sub characters {
     # are important inside, we just replace all \n by ' '
     # then all multiple white space by single ' '
     # then remove all ' ' at beginning or end
-    # and skip everything if we get empty $data 
-    
+    # and skip everything if we get empty $data
+
     # do some whitespace stripping:
     return if ($data =~ /\A\s*\Z/);
 
@@ -278,7 +281,7 @@ sub characters {
        # can happen because of newlines
        return;
        }
-       
+
     if (($state eq "quoting") ||
         ($state eq "orth") ||
 	($state eq "tr")) {
@@ -292,7 +295,7 @@ sub characters {
     if ($reverse_index && ($lastStartTag eq "tr")) {
 	$current_headword .= $data;
 	}
-	
+
     elsif (!$reverse_index && ($lastStartTag eq "orth")) {
        if (($preLastStartTag eq "form") ||
            ($preLastStartTag eq "orth")) {
@@ -310,58 +313,58 @@ sub end_element {
     my $part  = $element->{Name};
 
     pop @higherElements;
-    
+
 
 
     if (($state eq "quoting") || ($state eq "orth") || ($state eq "tr")) {
 	$quoted .= "</$part>\n";
 	}
-	
+
     if ($part eq "orth") {
 	Dict::add_headword($current_headword);
-        # Dict::end_headword() has to be called later, because we don't know now
+	# Dict::end_headword() has to be called later, because we don't know now
 	# if there is another orth coming (multiple headwords for one entry)
 	if($state eq "orth") {$state = "quoting"};
-	}	    
+	}
 
     if ($part eq "tr") {
 	Dict::add_headword($current_headword);
-        # Dict::end_headword() has to be called later, because we don't know now
+	# Dict::end_headword() has to be called later, because we don't know now
 	# if there is another tr coming
 	if($state eq "tr") {$state = "quoting"};
-	}	    
+	}
 
     elsif ($part eq "entry") {
         if ($reverse_index && $Dict::head) { Dict::end_headword() };
 
 	$quoted = apply_filter_cmd($filtercmd,$quoted);
-							      
-    	Dict::write_direct($quoted);	 
-        Dict::write_newline();
+
+	Dict::write_direct($quoted);
+	Dict::write_newline();
 	$state = "";
 	}
 
     elsif (($part eq "teiHeader")) {
-        return if $skip_header;
-	
+	return if $skip_header;
+
 	$state = "";
 
-        Dict::write_text();
-        Dict::write_newline();
+	Dict::write_text();
+	Dict::write_newline();
 
-        # 00-database-short	
+	# 00-database-short
 	Dict::set_headword(); Dict::add_text("00-database-short");
 	Dict::write_text(); Dict::end_headword();
-        Dict::write_direct("\n  ");
-        Dict::write_direct($database_short);
+	Dict::write_direct("\n  ");
+	Dict::write_direct($database_short);
 	if($reverse_index)
 	{
-          Dict::write_direct(' [reverse index]');
+	  Dict::write_direct(' [reverse index]');
 	}
-        Dict::write_newline();
+	Dict::write_newline();
 
 	## 00-database-info
-        Dict::set_headword();
+	Dict::set_headword();
 	$Dict::text="00-database-info";
 	Dict::write_text();
 	Dict::end_headword();
@@ -370,22 +373,36 @@ sub end_element {
 
 	Dict::add_text($quoted);# will do wrapping
 	Dict::write_text();
-        Dict::write_newline();
-	
+	Dict::write_newline();
+
 	# FIXME who defined 00-database-url and where is it used? it seems not in kdict
 	Dict::set_headword(); Dict::add_text("00-database-url");
 	Dict::write_text(); Dict::end_headword();
 	Dict::write_direct(" ");
 	Dict::write_direct($database_url);
-        Dict::write_newline();
-          
+	Dict::write_newline();
+
 	} # ^header
-	    
+
     elsif ($part eq "body") {
-        Dict::set_headword();# to have the last headword appear in INDEX
+	Dict::set_headword();# to have the last headword appear in INDEX
 	};
 
 }
 
+sub error
+{
+  my ($self, $args) = @_;
+  if ($args->{'Message'} =~ /^.+:.+:\d+:W:/)
+  {
+    # print warnings
+    print STDERR "Warning from XML::ESISParser: " . $args->{'Message'} . "\n";
+    return
+  }
+  use Data::Dumper;
+  print STDERR Dumper \@_;
+  print STDERR "Error from XML::ESISParser: " . $args->{'Message'} . "\n";
+  exit 3;
+}
 
 1;
--- tools/xmltei2xmldict.pl
+++ tools/xmltei2xmldict.pl
@@ -1,32 +1,37 @@
 #!/usr/bin/perl
-# $Id: xmltei2xmldict.pl,v 1.7 2005-08-14 10:41:05 micha137 Exp $
+# $Id: xmltei2xmldict.pl,v 1.8 2006-10-02 20:27:26 micha137 Exp $
 
-# V1.4 5/2004 Michael Bunk
-#   * added option to generate 00-database-allchars header 
+# V1.5 10/2006 Michael Bunk
+#   * removed -l option completely
+#   * cosmetic changes / rewrite
+#   * new option -n to set NSGMLS option of XML:ESISParser
 #
-# V1.3 4/2004 Michael Bunk kleinerurm-at-gmx.net
+# V1.4 5/2004 Michael Bunk
+#   * added option to generate 00-database-allchars header
+#
+# V1.3 4/2004 Michael Bunk kleinerwurm-at-gmx.net
 #   * finally used FindBin
 #   * use warnings; instead of #!perl -w to avoid warnings
 #     in foreign code (Sablotron module)
-# 
-# V1.1 2/2004 Michael Bunk kleinerurm-at-gmx.net
+#
+# V1.1 2/2004 Michael Bunk kleinerwurm-at-gmx.net
 #   * put .pm files into lib/
 #
-# V1.0 6/2003 Michael Bunk kleinerurm-at-gmx.net
+# V1.0 6/2003 Michael Bunk kleinerwurm-at-gmx.net
 #   * based on tei2dict_xml.pl
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software 
+# along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 use warnings;
@@ -39,82 +44,92 @@ use XML::Sablotron;
 use XML::Sablotron::DOM;
 use lib::TEIHandlerxml_xml;
 
-our ($opt_f, $opt_h, $opt_s, $opt_c, $opt_u, $opt_l, $opt_i,
-  $opt_r, $opt_t, $opt_a, $sab, $sit);
-getopts('surai:l:f:t:');
+our ($opt_f, $opt_h, $opt_s, $opt_c, $opt_u, $opt_i,
+  $opt_r, $opt_t, $opt_a, $sab, $sit, $opt_n, $opt_d);
+getopts 'surai:f:t:n:d:';
 
-if (!defined $opt_f) {
- print STDERR "\n$0 - convert Text Encoding Initiative files to\n";
- print STDERR " dictd database format keeping the xml in the <entry> elements\n\n";
- print STDERR " The TEI inputfile is expected as XML in TEI P4 format, see\n";
- print STDERR " http://www.tei-c.org/\n";
- print STDERR " Outputs .index and .dict file. The index is sorted with sort(1).\n";
- print STDERR " This help is outputted, because here was no tei file given.\n\n"; 
- print STDERR "Usage: $0 -f <teifile> [-sura] [-i <filtercmd>|-t <stylesheet.xsl>] [-l <locale>]\n";
- print STDERR " -s\t skip TEI header: do not treat it to generate\n";
- print STDERR "   \t  00-database-info & co special entrys (good to convert adapted\n";
- print STDERR "   \t  SGML tei files)\n";
- print STDERR " -u\t generate headword 00-database-utf8 in index file to\n";
- print STDERR "   \t  mark the database being in UTF-8 encoding\n";
- print STDERR "   \t  When this is given, 'sort' is called without -d option,\n";
- print STDERR "   \t  ie. all characters are used in comparisons\n";
- print STDERR " -r\t generate reverse index (use <tr> instead of <orth>)\n";
- print STDERR " -a\t generate headword 00-database-allchars (but no change in index mangling!)\n";
- print STDERR "   \t  This converter cannot generate the 00-database-alphabet entry, so -a is\n";
- print STDERR "   \t  required for non-latin scripts. You should prefer to use dictfmt(1) then.\n";
- print STDERR " -i <filtercmd>\t execute filtercmd for each entry (eg. 'sabcmd style.xsl')\n";
- print STDERR " -t <stylesheet.xsl>\t use an XSLT stylesheet for filtering the entries\n";
- print STDERR "   \t  with the Sablotron library. Excludes -i.\n";
-# this option exists in error - only byte ordering (through setting locale to
-# 'C' is correct for .index files
-# print STDERR " -l <locale>\t call 'sort' using <locale>. If not given, 'C' locale\n";
-# print STDERR "   \t  will be used ('C' locale for normal & utf-8 index).\n";
- print STDERR " <teifile>\t name of tei inputfile\n\n";
- die;
+unless (defined $opt_f)
+{
+ print STDERR <<END;
+
+$0 - convert Text Encoding Initiative files to
+ dictd database format, optionally keeping the xml in the <entry> elements
+
+
+ The TEI input file is expected as XML in TEI P4 format, see
+ http://www.tei-c.org/
+ Outputs .index and .dict file. The index is sorted with sort(1).
+ This help is printed, because there was no TEI file given.
+
+ Usage: $0 -f <teifile> [-sura] [-i <filtercmd>|-t <stylesheet.xsl>]
+           [-n <nsgmls>] [-d <declaration>]
+
+ -s   skip TEI header: do not treat it to generate 00-database-info & co
+      special entrys (good to convert adapted SGML TEI files)
+ -u   generate headword 00-database-utf8 in index file to mark the database
+      being in UTF-8 encoding.  When this is given, 'sort' is called without -d
+      option, ie. all characters are used in comparisons.
+ -r   generate reverse index (use <tr> instead of <orth>)
+ -a   generate headword 00-database-allchars (but no change in index mangling!)
+      This converter cannot generate the 00-database-alphabet entry, so -a is
+      required for non-latin scripts.  You should prefer to use dictfmt(1)
+      then.
+ -i <filtercmd>\t execute filtercmd for each entry (eg. 'sabcmd style.xsl')
+ -t <stylesheet.xsl>\t use an XSLT stylesheet for filtering the entries with
+      the Sablotron library. Excludes -i.
+ -n <nsgmls>\tSets the NSGMLS option of XML::ESISParser.  By default 'nsgmls'
+      is used.  To use OpenSP, set this option to 'onsgmls'.
+ -d <declaration>\tSets the Declaration option of XML::ESISParser.
+      XML::ESISParser expects the SGML declaration by default in
+      /usr/lib/sgml/declaration/xml.decl.  In Debian
+      /usr/share/xml/declaration/xml1n.dcl works best with nsgmls from the SP
+      XML parser.
+ <teifile>\t name of TEI input file
+
+END
+ exit 1
  }
- 
-our $file = $opt_f;
-die "Can't find file \"$file\"" unless -f $file;
 
-die "Only one of -i and -t may be given" if $opt_i && $opt_t;
+our $file = $opt_f;
+unless (-f $file)
+{ print STDERR "Can't find file \"$file\""; exit 2 }
+
+if ($opt_i && $opt_t)
+{ print STDERR "Only one of -i and -t may be given"; exit 2 }
 
 our $my_handler = lib::TEIHandlerxml_xml->new();
 
-$my_handler->set_options($file, # hand over name for .dict and .index output files
+$my_handler->set_options(
+    $file, # hand over name for .dict and .index output files
     $opt_s ? 1 : 0,		# skip TEI header
     $opt_u ? 1 : 0,		# generate 00-database-utf8
-    $opt_l ? $opt_l : "C",	# locale
+    "C",			# locale
     $opt_i ? $opt_i : "",	# filter command
     $opt_t,			# stylesheet for Sablotron
     $opt_r ? 1 : 0,		# generate reverse index
     $opt_a ? 1 : 0);		# generate 00-database-allchars
-    
-# XML::ESISParser should set these if IsSGML != 1
-$ENV{SP_ENCODING} = "XML";
-$ENV{SP_CHARSET_FIXED} = "YES";
 
-if (!defined($ENV{SGML_CATALOG_FILES})) {
- print STDERR "Warning: SGML_CATALOG_FILES is not set.\nPlease set it to point to\n";
- print STDERR " - the xml.soc file from the (Open)SP distribution\n";
- print STDERR " - and the TEI catalog file(s)\n";
- $ENV{SGML_CATALOG_FILES} = "/usr/share/doc/packages/sp/html-xml/xml.soc:".
- 	"/var/lib/sgml/CATALOG.tei_4xml:/var/lib/sgml/CATALOG.iso_ent";
- print STDERR "Using SGML_CATALOG_FILES=$ENV{SGML_CATALOG_FILES}\n";
+unless (defined $ENV{SGML_CATALOG_FILES})
+{
+ $ENV{SGML_CATALOG_FILES} =
+   "/var/lib/sgml/CATALOG.tei_4xml:/var/lib/sgml/CATALOG.iso_ent";
+ print STDERR <<END;
+Warning: The environment variable SGML_CATALOG_FILES is not set.
+Please point it to the TEI catalog file(s).
+
+Setting SGML_CATALOG_FILES=$ENV{SGML_CATALOG_FILES}
+END
  }
- 
-# FIXME the following schould be removed if we use XML,
-# but if we do, we get:
-# > XML::ESISParser::parse: unable to parse `file.tei'
-# > nsgmls:/usr/lib/sgml/declaration/xml.decl:1:W: SGML declaration was not implied
-# It seems ESISParser.pm is not supporting XML...
-our @additional_args;
-push (@additional_args, IsSGML => 1);
+
+our %additional_args;
+$additional_args{'Declaration'} = $opt_d if defined $opt_d;
+
+# Handling of the 'NSGMLS' option is broken in XML::ESISParser
+#$additional_args{'NSGMLS'} = $opt_n if defined $opt_n;
+$XML::ESISParser::NSGMLS_xml = $opt_n if defined $opt_n;
 
 XML::ESISParser->new->parse(Source => { SystemId => $file },
-                            Handler => $my_handler,@additional_args,
-#Declaration => "/usr/share/sgml/opensp/xml.dcl"
-  );
+                            Handler => $my_handler, %additional_args);
 
 print STDERR "Created $Dict::headwords headwords (including multiple <orth>-s / from the <tr>-s).\n";
 
-# EOF
