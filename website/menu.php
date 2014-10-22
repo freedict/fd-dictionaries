fb5ab50d75d3ec90eb4221d6d9c118de06a6a464
micha137 <micha137@e3372f6d-cdc2-4eab-8ac2-e1627c5befe3>
2014-10-22 08:41:07 +0000

    Server list moves to a new URL and host
    git-svn-id: https://svn.code.sf.net/p/freedict/code/trunk@1344 e3372f6d-cdc2-4eab-8ac2-e1627c5befe3

--- website/menu.php
+++ website/menu.php
@@ -60,7 +60,7 @@ require_once 'inc/gettext.php';
 
 
 <tr><td class="menu">
- <a href="http://ccls-online.de/dictd/index.php?freedictonly"
+ <a href="https://servers.freedict.org/?freedictonly"
   target="_top"><?php echo _('FreeDict Online Servers') ?></a>
 </td></tr>
 
