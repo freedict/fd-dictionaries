480af864bfff0c6bb84e0876f55d4ea159b1206f
Sebastian Humenda <shumenda |at| gmx |dot| de>
2015-12-31 18:25:38 +0100

    rework overview page
    -   correct a view grammatical issues
    -   add a few pieces of information
    -   add ToDo's :)

--- website/overview.php
+++ website/overview.php
@@ -11,20 +11,20 @@ $title = _('Overview'); require_once 'inc/head.php';
  '<a target="_top" href="http://sourceforge.net/project/memberlist.php?group_id=1419">',
  '</a>') ?></h2>
 
-<p><?php echo _('On this page you can find translating dictionary databases.
+<p><?php echo _('On this page you can find translation dictionary databases.
 The databases are free. That means they are available
-under the GNU General Public License or a less restrictive
-license. Check with the license of the respective database!') ?></p>
+under a free license like the GNU GPL or similar. Please check with the indrespective dictionary.') ?></p>
 
-<p><?php printf(_('The databases are available in XML. We support the conversion of FreeDict 
-databases from their native %4$sTEI%2$s format for use with %1$sdictd, the DICT dictionary server%2$s and other
-similar servers. Our own dictionary front-end can be found at %3$shttp://freedict.org/dict%2$s. Please note that
-on that page, you can also install a plugin that will let you query FreeDict dictionaries straight from your
+<p><?php printf(_('The databases are available in TEI, a XML language to encode human language. This way the dictionaries can be used both by humans and machines.
+We support the conversion of FreeDict databases from their native %4$sTEI%2$s
+format for use with %1$sdictd, the DICT dictionary server%2$s and other
+similar servers. Our own online dictionary lookup page can be found at %3$shttp://freedict.org/dict%2$s. Please note that
+you can also install a plugin on that page which will let you query FreeDict dictionaries straight from your
 browser search window.'), '<a href="http://www.dict.org/w/" target="_parent">', '</a>',
 '<a href="http://freedict.org/dict" target="_top">', '<a href="http://www.tei-c.org/" target="_top">') ?></p>
 
-<p><?php echo _('But there are other applications for our databases as well. You could use
-them to generate wordlists for spellcheckers and to create new dictionaries, to import them 
+<p><?php echo _('There are other applications for our databases as well. It is
+possible to use them to generate wordlists for spellcheckers and to create new dictionaries, to import them 
 into your own terminological database and even to print your own dictionary with XSL-FO.') ?></p>
 
 <p><?php echo _('If you would like to see any other language included,
@@ -38,9 +38,20 @@ width="88" height="31" border="0" align="left" alt="SourceForge Logo" /></a>
 Please support them also. Without SourceForge, FreeDict would
 not have come so far!') ?></p>
 
-<p><?php echo _('This project was started in 2000 by Horst Eyermann. The databases
+<h2><?php echo _('History') ?></p>
+<p><?php echo _('This project was started in 2000 by Horst Eyermann. 
+TODO: document his work...
+The first databases were derived from Ergane.') ?></p>
+
+<!-- TODO: document michael's work
+
+TODO: Piotr
+-->
+
+<!-- where to put this:
+The databases
 are a compilation of various free sources. Please consult the
 respective TEI headers, READMEs and 00-database-info entries.
-The first databases were derived from Ergane.') ?></p>
+-->
 
 <? require_once 'inc/footer.php';
